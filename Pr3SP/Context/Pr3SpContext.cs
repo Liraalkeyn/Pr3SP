af1fdc38b74a18c8d3c9067f8c2702f00c57d8a1
Liraalkeyn <mikunar2018@gmail.com>
2024-04-14 14:48:47 +0300

    ref

---
+++ Pr3SP/Context/Pr3SpContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pr3SP.Models;
+
+namespace Pr3SP.Context;
+
+public partial class Pr3SpContext : DbContext
+{
+    public Pr3SpContext()
+    {
+    }
+
+    public Pr3SpContext(DbContextOptions<Pr3SpContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=Pr3SP;Username=postgres;Password=postgres");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("User_pkey");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.Patronymic).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Pr3SP/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pr3SP.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public DateOnly Birth { get; set; }
+
+    public string? Patronymic { get; set; }
+}
